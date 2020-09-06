606129437125a353b71ab2ccd23e2bbd008005ec
Paweł Uszakow <51965778+uszakow@users.noreply.github.com>
2020-09-06 18:45:13 +0200

    Updates

---
+++ precache-manifest.eb4f65c2bdba2b844c23cbfd94bcecb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7af62021d624c7cca4e392abdd97f491",
+    "url": "/2020-04-Pokemons/index.html"
+  },
+  {
+    "revision": "f760aaad9502cbe3518f",
+    "url": "/2020-04-Pokemons/static/css/main.5e780c6b.chunk.css"
+  },
+  {
+    "revision": "e2dfcc3739427344fa8e",
+    "url": "/2020-04-Pokemons/static/js/2.675d05c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2020-04-Pokemons/static/js/2.675d05c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f760aaad9502cbe3518f",
+    "url": "/2020-04-Pokemons/static/js/main.252fde38.chunk.js"
+  },
+  {
+    "revision": "0062de99c0c5825b3fff",
+    "url": "/2020-04-Pokemons/static/js/runtime-main.1daf8589.js"
+  }
+]);
